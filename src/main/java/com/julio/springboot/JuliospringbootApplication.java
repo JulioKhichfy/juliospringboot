ea484da1e8e964c63e82ca31176c6fbdb9ba1ac2
julio khichfy <juliocesark@gmail.com>
2019-01-20 17:51:47 -0200

    Projeto criado

---
+++ src/main/java/com/julio/springboot/JuliospringbootApplication.java
@@ -0,0 +1,14 @@
+package com.julio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuliospringbootApplication.class, args);
+	}
+
+}
+
