ea29b3e737376134471b16efa2f41c799c5c565b
julio khichfy <juliocesark@gmail.com>
2019-01-20 21:49:09 -0200

    Adicionando Entidades de Domínio: Estado e Cidade. Ciclo completo

--- src/main/java/com/julio/springboot/JuliospringbootApplication.java
+++ src/main/java/com/julio/springboot/JuliospringbootApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.julio.springboot.domain.Categoria;
+import com.julio.springboot.domain.Cidade;
+import com.julio.springboot.domain.Estado;
 import com.julio.springboot.domain.Produto;
 import com.julio.springboot.repositories.CategoriaRepository;
+import com.julio.springboot.repositories.CidadeRepository;
+import com.julio.springboot.repositories.EstadoRepository;
 import com.julio.springboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class JuliospringbootApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JuliospringbootApplication.class, args);
@@ -46,6 +56,19 @@ public class JuliospringbootApplication implements CommandLineRunner{
 		//A ORDEM AQUI NÃO INTERFERE. MÃO DUPLA
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
 	}
