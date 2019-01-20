7b69eebf6dc11a39f632481cf97505787587b0af
julio khichfy <juliocesark@gmail.com>
2019-01-20 19:52:06 -0200

    upgrade da versao do springboot para 2.1.2 e criação do Repository e Service para Categoria

--- src/main/java/com/julio/springboot/resources/CategoriaResource.java
+++ src/main/java/com/julio/springboot/resources/CategoriaResource.java
@@ -1,26 +1,25 @@
 package com.julio.springboot.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.julio.springboot.domain.Categoria;
+import com.julio.springboot.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(1, "informatica");
-		Categoria c2 = new Categoria(2, "escritorio");
-		
-		List<Categoria> categorias = new ArrayList<Categoria>();
-		categorias.add(c1);
-		categorias.add(c2);
-		return categorias;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
