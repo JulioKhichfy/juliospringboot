cf4b1c4bc3dbf4147a2715f03a8020807def771e
julio khichfy <juliocesark@gmail.com>
2019-01-20 20:23:42 -0200

    Criação da classe Produto e sua relação N:N com Categoria

--- src/main/java/com/julio/springboot/JuliospringbootApplication.java
+++ src/main/java/com/julio/springboot/JuliospringbootApplication.java
@@ -8,24 +8,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.julio.springboot.domain.Categoria;
+import com.julio.springboot.domain.Produto;
 import com.julio.springboot.repositories.CategoriaRepository;
+import com.julio.springboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class JuliospringbootApplication implements CommandLineRunner{
 
 	@Autowired
-	private CategoriaRepository categoriaRepository; 
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JuliospringbootApplication.class, args);
 	}
 
-	//CommandLineRunner para popular a tabela CATEGORIA
+	//CommandLineRunner para popular as tabelas no INIT
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));//método novo SaveAll para spring2.x.x
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		//A ORDEM AQUI NÃO INTERFERE. MÃO DUPLA
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
